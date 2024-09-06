c80569b99cbf9c860980457e1c628af05307267f
Kevin <kevinchanchi02@gmail.com>
2024-09-06 15:54:17 -0500

    backup_30-08-2024

---
+++ backups/Export_30-08-2024.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bd_prueba
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `compradores`
+--
+
+DROP TABLE IF EXISTS `compradores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compradores` (
+  `id` int NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `nit` varchar(20) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compradores`
+--
+
+LOCK TABLES `compradores` WRITE;
+/*!40000 ALTER TABLE `compradores` DISABLE KEYS */;
+INSERT INTO `compradores` VALUES (2,'Tecnología Innovadora S.A.','900123456-7','contacto@tecnologiainnovadora.com','6015556789','2024-08-28 06:30:00','2024-08-28 06:30:00'),(3,'María Rodríguez','','rodriguez123@gmail.com','3207654321','2024-08-28 06:48:00','2024-08-30 05:20:00'),(4,'Consultores Financieros Ltda.','800987654-3','info@consultoresfinancieros.com','6024445566','2024-08-28 07:00:00','2024-08-28 07:00:00');
+/*!40000 ALTER TABLE `compradores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-08-30 22:34:22
