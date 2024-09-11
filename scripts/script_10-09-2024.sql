c4bf1139868f9f3b027cf9f7cb07d4426204834f
Kevin <kevinchanchi02@gmail.com>
2024-09-10 23:39:50 -0500

    Creacion y relacion de entidades pais,departamento,ciudad. Relacion ciudad-proveedor

---
+++ scripts/script_10-09-2024.sql
@@ -0,0 +1,77 @@
+#Creacion de la base de datos "proyecto_dev_bd".
+
+drop database if exists proyecto_dev_bd;
+
+create database proyecto_dev_bd;
+
+use proyecto_dev_bd;
+
+#creacion de la entidad pais con sus atributos.
+create table pais (
+	IDPais int primary key auto_increment,
+    pais varchar(50) not null,
+    createdAt datetime not null,
+    updatedAt datetime not null
+);
+
+#creacion de la entidad departamento con sus atributos y relacion pais-departamento.
+create table departamento (
+	IDDepartamento int primary key auto_increment,
+    departamento varchar(40) not null,
+    IDPais int not null,
+    createdAt datetime not null,
+    updatedAt datetime not null,
+    foreign key(IDPais) references pais(IDPais)
+);
+
+#creacion entidad ciudad con sus atributos y relacion departamento-ciudad.
+create table ciudad (
+	IDCiudad int primary key auto_increment,
+    ciudad varchar(50) not null,
+    IDDepartamento int not null,
+    createdAt datetime not null,
+    updatedAt datetime not null,
+    foreign key(IDDepartamento) references departamento(IDDepartamento)
+);
+
+#creacion de la entidad categoria con sus atributos.
+create table categoria (
+	IDCategoria int primary key auto_increment,
+    nombreCategoria varchar(50) not null,
+    descripcion text,
+    createdAt datetime not null,
+    updatedAt datetime not null
+);
+
+#creacion entidad proveedor con sus atributos y relacion ciudad-proveedor
+create table proveedor (
+	IDProveedor int primary key auto_increment,
+    nombresProveedor varchar(40),
+    apellidoPaterno varchar(40),
+    apellidoMaterno varchar(40),
+    razonSocial varchar(50),
+    nit varchar(15),
+    direccion varchar(80),
+    email varchar(80),
+    telefono varchar(12) not null,
+    IDCiudad int not null,
+    createdAt datetime not null,
+    updatedAt datetime not null,
+    foreign key(IDCiudad) references ciudad(IDCiudad)
+);
+
+#creacion entidad producto con sus atributos.
+#se relacionan las entidades producto-categoria y producto-proveedor.
+create table producto (
+	IDProducto int primary key auto_increment,
+    nombreProducto varchar(50) not null,
+    descripcion text,
+    IDCategoria int not null,
+    IDProveedor int not null,
+    cantidadStock int not null,
+    precioUnitario decimal(10, 2) not null,
+    createdAt datetime not null,
+    updatedAt datetime not null,
+    foreign key(IDCategoria) references categoria(IDCategoria),
+    foreign key(IDProveedor) references proveedor(IDProveedor)
+);
